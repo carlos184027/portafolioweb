0ff08cc370b6de8425f82ba20b121a89e9b1b42f
carlos184027 <160245802+carlos184027@users.noreply.github.com>
2024-05-05 11:13:08 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,31 @@
+// Variable para mantener el índice del slide actual
+let slideIndex = 0;
+
+// Función para mostrar los slides
+function showSlides() {
+  // Selecciona todas las imágenes dentro del contenedor del carrusel
+  const slides = document.querySelectorAll('.carousel-content img');
+  
+  // Itera sobre todas las imágenes y las oculta
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove('active');
+  }
+
+  // Incrementa el índice del slide
+  slideIndex++;
+  
+  // Si el índice es mayor que el número de slides, vuelve al primero
+  if (slideIndex > slides.length) { 
+    slideIndex = 1; 
+  }
+  
+  // Muestra el slide actual
+  slides[slideIndex - 1].classList.add('active');
+
+  // Llama a la función showSlides de nuevo después de 2 segundos
+  setTimeout(showSlides, 2000); 
+}
+
+// Llama a la función showSlides para iniciar el carrusel
+showSlides();
+
